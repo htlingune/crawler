49e6c5c0e11cbe0497b5cc4aec3669df0d43ee85
htlingune <59502548+htlingune@users.noreply.github.com>
2020-02-18 15:09:58 +0800

    Add files via upload
    for 'https://ec.ltn.com.tw/list/securities'

---
+++ ltn_securities.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import json
+import re
+import time
+headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36'}
+path =r'./ltn_securities/%s'
+if not os.path.exists(r'./ltn_securities'):
+    os.mkdir(r'./ltn_securities')
+url_list = 'https://ec.ltn.com.tw/list/securities'
+session = requests.session()
+response_head = session.get(url_list, headers = headers)
+soup_list = BeautifulSoup(response_head.text, 'html.parser')
+finalpage = soup_list.select('div[class="pagination boxTitle"]  a[data-desc="最後一頁"]')[0]['href']
+finalpage_number = re.search(r"\d+",finalpage)[0]
+session.close()
+for i in range(1,int(finalpage_number)):
+    url_indexed_list = 'https://ec.ltn.com.tw/list/securities/' + str(i)
+    response_list = session.get(url_indexed_list, headers=headers)
+    soup = BeautifulSoup(response_list.text, 'html.parser')
+    for k in range(len(soup.select('div[data-desc="文章列表"] a[class="boxText"] div[class="tit"] p'))):
+        href = soup.select('div[data-desc="文章列表"] a[class="boxText"]')[k]['href']
+        title = soup.select('div[data-desc="文章列表"] a[class="boxText"] div[class="tit"] p')[k].text.replace('/','_').replace('<',' ').replace('>',' ').replace('/','').replace('＃','').replace('?','').replace("\r",'_')
+        date = soup.select('div[data-desc="文章列表"] a[class="boxText"] div[class="tit"] span')[k].text
+        clicks = "NA"
+        tag = "NA"
+        content_text = ''
+        content_response = session.get(href, headers = headers)
+        content_soup = BeautifulSoup(content_response.text, 'html.parser')
+        for j in content_soup.select('p'):
+            if len(j.text) >2:
+                content_text += j.text
+        output = {'date': date, 'title': title, 'content': content_text, 'href': href, 'tag': tag, 'clicks': clicks}
+        with open(path % (title) + '.json', 'w', encoding='utf8') as f:
+            json.dump(output, f)
+        time.sleep(1)
