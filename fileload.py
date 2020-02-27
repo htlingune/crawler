f3b6495b6f52d97fbdeb1dc636df95de14f92728
htlingune <59502548+htlingune@users.noreply.github.com>
2020-02-27 18:42:30 +0800

    Update fileload.py

--- fileload.py
+++ fileload.py
@@ -4,6 +4,10 @@ import pandas as pd
 with open(r"filepath",'r',encoding= 'utf-8') as load_f:
     load_dict = json.load(load_f)
     print(load_dict)
-
+	content = load_dict['content']
+    content = re.sub(r"((<a)(.+?)(a>))",'',load_dict['content'],count=0)
+    content = re.sub(r'(\\n)','',load_dict['content'],count=0)
+    content = re.sub(r'(&l)(.+?)(gt;)','',load_dict['content'],count=0)
+    print(content)
 df = pd.read_json(r"filepath")
 
