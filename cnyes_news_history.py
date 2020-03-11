df4b9f096e44ef779867b10518c582f5043369f7
htlingune <59502548+htlingune@users.noreply.github.com>
2020-03-11 16:11:50 +0800

    update

--- cnyes_news_history.py
+++ cnyes_news_history.py
@@ -4,7 +4,43 @@ import json
 import time
 import datetime
 import re
-year = int
+
+def req_json(url,headers):
+    response = session.get(url, headers=headers)
+    json = response.json()
+    return json
+
+def content(json,n):
+    title = str(json['items']['data'][n]['title']).replace('<', ' ').replace('>', ' ').replace('/', '').replace('＃', '').replace('?', '').replace('*', '_').replace('\"', '_').replace(':', '_').replace('\n', '_')
+    article_id = str(json['items']['data'][n]['newsId'])
+    href = 'https://news.cnyes.com/news/id/' + article_id
+    content_url = 'https://news.cnyes.com/api/v6/news/' + article_id
+    date = time.ctime(json['items']['data'][n]['publishAt'])
+    content_response = session.get(content_url, headers=headers)
+    content_json = content_response.json()
+    content = content_json['items']['content']
+    tag = content_json['items']['keywords']
+    content = text_clean(content)
+    output = {'date': date, 'title': title, 'content': content, 'href': href, 'tag': tag}
+    return title, output
+
+def text_clean(content):
+    if re.search(r'(<a.+?a>)', content) != None:
+        content = re.sub(r"(\(<a.+?a>\))", '', content, count=0, flags=re.IGNORECASE)
+    if re.search(r'(&l.+?gt;)', content) != None:
+        content = re.sub(r'(&l.+?gt;)', '', content, count=0)
+    if re.search(r'(&a.+?sp;)', content) != None:
+        content = re.sub(r'(&a.+?sp;)', '', content, count=0)
+    if re.search(r'(\n)', content) != None:
+        content = content.replace('\r', '')
+        content = content.replace('\n', '')
+    return content
+
+def file_save(path,title):
+    with open(path % (title) + '.json', 'w', encoding='utf8') as f:
+        json.dump(output, f)
+
+year = 2015
 path = r'./cnyesnewshistory'+r'/'+str(year)+r'/%s'
 if not os.path.exists(r'./cnyesnewshistory'+r'/'+str(year)):
     os.mkdir(r'./cnyesnewshistory'+r'/'+str(year))
@@ -16,37 +52,15 @@ for i in range(1,12):
     timerange = [starttime,endtime]
     url_news_index = 'https://news.cnyes.com/api/v3/news/category/tw_stock?startAt=%s&endAt=%s' %(timerange[0],timerange[1])
     session = requests.session()
-    response = session.get(url_news_index, headers = headers)
-    index_json = response.json()
+    index_json = req_json(url_news_index, headers)
     lastpage = int(index_json['items']['last_page'])
     session.close()
     for l in range(1,lastpage+1):
-        url_news_page = 'https://news.cnyes.com/api/v3/news/category/tw_stock?startAt=%s&endAt=%s&page=%d' % (
-        timerange[0], timerange[1], l)
-        response_news = session.get(url_news_page, headers = headers)
-        news_page_json = response_news.json()
+        url_news_page = 'https://news.cnyes.com/api/v3/news/category/tw_stock?startAt=%s&endAt=%s&page=%d' % (timerange[0], timerange[1], l)
+        news_page_json = req_json(url_news_page, headers)
         data_perpage = len(news_page_json['items']['data'])
         for n in range(data_perpage):
-            title = str(news_page_json['items']['data'][n]['title']).replace('<',' ').replace('>',' ').replace('/','').replace('＃','').replace('?','').replace('*','_').replace('\"','_').replace(':','_').replace('\n','_')
+            title, output = content(news_page_json, n)
             if not os.path.exists(path %(title)):
-                article_id = str(news_page_json['items']['data'][n]['newsId'])
-                content_url = 'https://news.cnyes.com/api/v6/news/' + article_id
-                date = time.ctime(news_page_json['items']['data'][n]['publishAt'])
-                content_response = session.get(content_url, headers = headers)
-                content_json = content_response.json()
-                content = content_json['items']['content']
-		if re.search(r'(<a.+?a>)', content) != None:
-                    content = re.sub(r"(\(<a.+?a>\))", '', content, count=0, flags=re.IGNORECASE)
-                if re.search(r'(&l.+?gt;)', content) != None:
-                    content = re.sub(r'(&l.+?gt;)', '', content, count=0)
-                if re.search(r'(&a.+?sp;)', content) != None:
-                    content = re.sub(r'(&a.+?sp;)', '', content, count=0)
-                if re.search(r'(\n)', content) != None:
-                    content = content.replace('\r', '')
-                    content = content.replace('\n', '')
-                tag = content_json['items']['keywords']
-                href = 'https://news.cnyes.com/news/id/' + article_id
-                output = {'date':date,'title':title,'content':content,'href':href,'tag':tag}
                 session.close()
-                with open(path %(title) + '.json', 'w', encoding='utf8') as f:
-                    json.dump(output,f)
+                file_save(path, title)
